862d646c8ca16b95765468a1607e596047ac25bb
djwang88 <63133956+djwang88@users.noreply.github.com>
2020-09-13 18:51:25 -0600

    Updated ending code with new code

--- endcode.js
+++ endcode.js
@@ -2,7 +2,7 @@
  * Generator and Gecko code developed by djwang88
  */
 
-const code = "C21C4344 00000003\n2C0300XX 40820008\n38600001 3803FFFF\n60000000 00000000";
+const code = "041C427C 380000XX";
 
 var resultBox = document.querySelector('#result');
 var targetsBox = document.querySelector('#num-targets');
@@ -14,7 +14,7 @@ function generate() {
 		return;
 	}
 
-	var replacement = (numTargets + 1).toString(16).padStart(2, '0').toUpperCase();
+	var replacement = numTargets.toString(16).padStart(2, '0').toUpperCase();
 	var result = code.replace("XX", replacement);
 	resultBox.value = result;
 }
